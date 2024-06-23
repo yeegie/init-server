846b94a6835c8165057cb3ae612055a0648c1c99
yeegie <lotus9200@gmail.com>
2024-06-24 01:21:28 +0600

    fix

--- init.sh
+++ init.sh
@@ -34,8 +34,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Power 10k
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 cat << EOT >> ~/.zshrc
 export ZSH=$HOME/.oh-my-zsh
